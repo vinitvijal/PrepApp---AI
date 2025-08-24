d214b602687fa488e31b067451fd05bc25f21bed
Vinu <vineet.22566@sscbs.du.ac.in>
2025-08-24 22:45:51 +0530

    Clerk added for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { ClerkProvider } from '@clerk/nextjs'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
-      >
-        <Header/>
-        {children}
-      </body>
+      <ClerkProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
+          >
+          <Header/>
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }

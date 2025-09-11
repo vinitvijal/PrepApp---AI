a4b2672e89a2494faface18d60e2d527ecf16834
Vinu PC <vineet.22566@sscbs.du.ac.in>
2025-09-11 14:23:03 +0530

    Header added

--- app/(home)/resume-manager/page.tsx
+++ app/(home)/resume-manager/page.tsx
@@ -1,11 +1,24 @@
+import { Upload } from 'lucide-react'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      <h1>Resume Manager Page</h1>
-    </div>
+    <main className=' h-screen bg-white'>
+      <Header/>
+    </main>
   )
+
+  function Header() {
+    return <header className=' w-full flex flex-col md:flex-row md:justify-between items-center px-8  py-4 bg-zinc-100'>
+      <div>
+        <h1 className=' text-black text-3xl'>RESUME MANAGER</h1>
+        <p className=' text-zinc-700 text-xs'>ATS SCORING • VERSIONING • SHARING</p>
+      </div>
+      <div>
+        <button className=' bg-green-500 py-2 px-4 flex'><Upload /> Upload Resumhhe</button>
+      </div>
+    </header>
+  }
 }
 
 export default page
